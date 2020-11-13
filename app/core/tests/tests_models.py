6d249d15700e81cc89e90602d70ab673160f1080
Ivan Rylov <ivan.rylov@travelperk.com>
2020-11-13 12:14:17 +0100

    add admin and user models and tests

---
+++ app/core/tests/tests_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_sucessful(self):
+        """Test creating a user with an email is successful"""
+        email = 'test@gmail.com'
+        password = 'test123'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for new user is normalized"""
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_emails(self):
+        """Test new user creating with no email raised error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creation of superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@gmail.com",
+            "123test"
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
